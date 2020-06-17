a4b79954400d7e2540f76177899e6929b6bfdd24
Papuna Gaprindashvili <pgapr14@freeuni.edu.ge>
2020-06-17 11:46:54 +0400

    messaging implemented

--- traki/src/router/index.js
+++ traki/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +7,7 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
   },
   {
     path: '/about',
@@ -17,7 +16,15 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/componentDemo',
+    name: 'ComponentDemo',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "componentdemo" */ '../views/ComponentDemo.vue')
+  },
 ]
 
 const router = new VueRouter({
---
+++ traki/src/views/ComponentDemo.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h1>
+      this is component test page that implements basic chat
+    </h1>
+    <div>
+      <ul>
+        <li 
+          v-for="(message, index) in messages"
+          :key="message.body + index"
+        >
+          person {{message.senderId}} says {{message.body}}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <div>
+        <form v-on:submit.prevent="sendMessage(1)">
+          <input type="text" v-model="messageInput1">
+          <button type="submit">გაგზავნა</button>
+        </form>
+      </div>
+      <div>
+        <form v-on:submit.prevent="sendMessage(2)">
+          <input type="text" v-model="messageInput2">
+          <button type="submit">გაგზავნა</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentDemo',
+  data: function () {
+    return {
+      messageInput1: '',
+      messageInput2: '',
+      messages: []
+    }
+  },
+  methods: {
+    sendMessage (senderId) {
+      console.log('message sent by user id: ' + senderId)
+      if (senderId === 1) {
+        this.messages.push({
+          senderId: senderId,
+          body: this.messageInput1,
+        })
+        this.messageInput1 = ''
+      } else if (senderId === 2) {
+        this.messages.push({
+          senderId: senderId,
+          body: this.messageInput2,
+        })
+        this.messageInput2 = ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
