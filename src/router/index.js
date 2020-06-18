e1fa7ae8d751eae6e73f4971fa8f13eb627f37c6
Papuna Gaprindashvili <pgapr14@freeuni.edu.ge>
2020-06-18 12:26:01 +0400

    add Table Test Component

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,15 @@ Vue.use(VueRouter)
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "componentdemo" */ '../views/ComponentDemo.vue')
   },
+  {
+    path: '/tablesDemo',
+    name: 'TablesDemo',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "tablesdemo" */ '../views/TablesDemo.vue')
+  },
+  
 ]
 
 const router = new VueRouter({
